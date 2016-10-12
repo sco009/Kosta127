ac551d298d1b95b00a5c86d2d477542334f5f98d
kibeom <sco009@naver.com>
2016-10-12 13:43:59 +0900

    123

---
+++ Project/src/cosmos/multiple/model/MultipleChoice.java
@@ -0,0 +1,44 @@
+package cosmos.multiple.model;
+
+import java.io.Serializable;
+
+public class MultipleChoice implements Serializable {
+	String multipleChoiceId;
+	String multipleChoiceOne;
+	String multipleChoiceTwo;
+	String multipleChoiceThree;
+	String multipleChoiceFour;
+	public String getMultipleChoiceId() {
+		return multipleChoiceId;
+	}
+	public void setMultipleChoiceId(String multipleChoiceId) {
+		this.multipleChoiceId = multipleChoiceId;
+	}
+	public String getMultipleChoiceOne() {
+		return multipleChoiceOne;
+	}
+	public void setMultipleChoiceOne(String multipleChoiceOne) {
+		this.multipleChoiceOne = multipleChoiceOne;
+	}
+	public String getMultipleChoiceTwo() {
+		return multipleChoiceTwo;
+	}
+	public void setMultipleChoiceTwo(String multipleChoiceTwo) {
+		this.multipleChoiceTwo = multipleChoiceTwo;
+	}
+	public String getMultipleChoiceThree() {
+		return multipleChoiceThree;
+	}
+	public void setMultipleChoiceThree(String multipleChoiceThree) {
+		this.multipleChoiceThree = multipleChoiceThree;
+	}
+	public String getMultipleChoiceFour() {
+		return multipleChoiceFour;
+	}
+	public void setMultipleChoiceFour(String multipleChoiceFour) {
+		this.multipleChoiceFour = multipleChoiceFour;
+	}
+	
+	
+	
+}
